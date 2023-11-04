5df172e451b34eb1bc640d7c1a3fe2f3d2eb6223
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2023-11-04 12:30:08 +0100

    🔄 Chore (app.json): Oppdater versjonsnummer til 0.1.0 🔄 Chore (app.json): Oppdater kategori til "internet" 🔄 Chore (app.json): Oppdater brandColor til "#081822" 🔄 Chore (app.json): Oppdater drivere til å inkludere "cloud" tilkobling 🔄 Chore (app.json): Oppdater drivere for å inkludere "getPolestar" handling 🔄 Chore (icon.svg): Oppdater ikon.svg 📸 Add (assets/images/large.png): Legg til large.png bilde 📸 Add (assets/images/small.png): Legg til small.png bilde 📸 Add (assets/images/xlarge.png): Legg til xlarge.png bilde
    📦 Chore (driver.compose.json): Oppdater tilkoblingsinnstilling for Polestar-driveren
    🐛 Fix (driver.js): Fiks feil i onPair-metoden for Polestar-driveren
    ✨ Feat (pair/getPolestar.html): Legg til skript for å hente Polestar 2 fra Tibber
    🐛 Fix (pair/start.html): Fiks visningsfeil i login-skjemaet

--- drivers/polestar-2/driver.js
+++ drivers/polestar-2/driver.js
@@ -6,6 +6,8 @@ const axios = require('axios');
 class Polestar extends Driver {
 	async onInit() {
 		this.log('Polestar has been initialized');
+
+		this.token = null;
 	}
 
 	async onPair(session) {
@@ -18,11 +20,33 @@ class Polestar extends Driver {
 					email,
 					password,
 				});
+
 				const { data: { token } } = response;
-				return token;
+				this.token = token;
+
+				return { success: true, token };
 			} catch (error) {
 				this.error(error);
-				return null;
+				return error;
+			}
+		});
+
+		session.setHandler('getVehicles', async (data) => {
+			try {
+				const response = await axios.post('https://app.tibber.com/v4/gql', {
+					query: '{\n  me{\n    myVehicles{\n      vehicles{\n        id\n        title\n        }\n    }\n  }\n}',
+				}, {
+					headers: {
+						'Authorization': `Bearer ${this.token}`,
+						'Content-Type': 'application/json',
+						'Accept': 'application/json',
+					},
+				});
+				const { data: { data: { me: { myVehicles: { vehicles } } } } } = response;
+				return { success: true, vehicles };
+			} catch (error) {
+				this.error(error);
+				return error;
 			}
 		});
 	}
