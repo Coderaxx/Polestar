2ff762521b32da1a0f66e1a973337488aa3164c5
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2024-01-05 08:54:26 +0100

    Remove PS2 driver files and update settings page

--- drivers/ps2/device.js
+++ drivers/ps2/device.js
@@ -1,99 +1,97 @@
 'use strict';
 
 const { Device } = require('homey');
-const axios = require('axios');
 const moment = require('moment');
-const PolestarAPI = require('./PolestarAPI');
+const Polestar = require('@andysmithfal/polestar.js');
+const HomeyCrypt = require('../../lib/homeycrypt');
 
-class PolestarBetaDevice extends Device {
+class PS2Device extends Device {
     async onInit() {
         this.name = this.getName();
 
         this.homey.app.log(this.homey.__({
             en: `${this.name} has been initialized`,
             no: `${this.name} har blitt initialisert`
-        }),
-            this.name, 'DEBUG');
+        }), this.name, 'DEBUG');
 
         moment.locale(this.homey.i18n.getLanguage() == 'no' ? 'nb' : 'en');
 
         this.settings = await this.getSettings();
-        this.auth = {
-            email: await this.homey.settings.get('polestar_email') || null,
-            password: await this.homey.settings.get('polestar_password') || null,
-        }
-        this.token = await this.homey.settings.get('polestar_token') || null;
-        this.isLoggedIn = this.token !== null;
-        this.vehicleId = this.getData().id;
-        this.vehicleData = null;
-        this.PolestarAPI = new PolestarAPI(this.auth.email, this.auth.password, this.vehicleId, 'a0c41700ca56416b8c2ad7c9028c1291');
-        try {
-            const { accessToken, refreshToken, tokenType } = await this.PolestarAPI.getAccessToken();
-            this.token = accessToken;
-            this.homey.settings.set('polestar_token', this.token);
-        } catch (error) {
-            this.homey.app.log(this.homey.__({
-                en: 'Error logging in to Polestar',
-                no: 'Feil ved innlogging til Polestar'
-            }),
-                this.name, 'ERROR', error);
-        }
+        await this.fixCapabilities();
+        this.update_loop_timers();
+    }
 
-        this.abortController = null;
+    update_loop_timers() {
+        this.updateVehicleState();
+        let interval = 60000;
+        this._timerTimers = setInterval(() => {
+            this.updateVehicleState();
+        }, interval);
+    }
 
-        this.refreshInterval = this.settings.refresh_interval * 60 * 1000 || 60 * 60 * 1000;
+    async fixCapabilities() {
+        if (!this.hasCapability('measure_battery'))
+            await this.addCapability('measure_battery');
+        // if(!this.hasCapability('measure_current'))
+        //   await this.addCapability('measure_current');
+        // if(!this.hasCapability('measure_power'))
+        //   await this.addCapability('measure_power');
+        if (!this.hasCapability('measure_polestarChargeTimeRemaining'))
+            await this.addCapability('measure_polestarChargeTimeRemaining');
+        if (!this.hasCapability('measure_polestarOdometer'))
+            await this.addCapability('measure_polestarOdometer');
+        if (!this.hasCapability('measure_polestarRange'))
+            await this.addCapability('measure_polestarRange');
+        if (!this.hasCapability('measure_polestarChargeState'))
+            await this.addCapability('measure_polestarChargeState');
+        if (!this.hasCapability('measure_polestarConnected'))
+            await this.addCapability('measure_polestarConnected');
 
-        this.homey.app.log(this.homey.__({
-            en: `Interval set to ${this.refreshInterval / 1000 / 60} minutes`,
-            no: `Intervall satt til ${this.refreshInterval / 1000 / 60} minutter`
-        }),
-            this.name, 'DEBUG');
-        await this.updateDeviceData();
-        this.interval = this.homey.setInterval(async () => {
-            await this.updateDeviceData();
-        }, this.refreshInterval);
     }
 
     async updateDeviceData() {
         this.homey.app.log(this.homey.__({
             en: 'Updating device data for ' + this.name,
             no: 'Oppdaterer enhetsdata for ' + this.name
-        }),
-            this.name, 'DEBUG');
-        this.vehicleData = await this.PolestarAPI.update();
+        }), this.name, 'DEBUG');
 
-        if (this.vehicleData) {
-            const lastUpdated = moment(this.vehicleData.batteryChargeLevel.timestamp).fromNow();
-            const soc = parseInt(this.vehicleData.batteryChargeLevel.value);
-            const range = this.vehicleData.electricRange.value;
-
-            await this.setCapabilityValue('measure_battery', soc);
-            await this.setCapabilityValue('measure_polestarBattery', soc);
-            await this.setCapabilityValue('measure_polestarRange', `${range} km`);
-            await this.setCapabilityValue('measure_polestarChargeState', this.vehicleData.chargingConnectionStatus.value === null
-                ? this.homey.__('Polestar2.device.unknownChargingState')
-                : this.vehicleData.chargingConnectionStatus.value !== 'CONNECTION_STATUS_DISCONNECTED'
-                    ? this.homey.__('Polestar2.device.isCharging')
-                    : this.homey.__('Polestar2.device.isNotCharging'));
-            await this.setCapabilityValue('measure_polestarUpdated', lastUpdated);
-        } else {
-            this.homey.app.log(this.homey.__({
-                en: 'Failed to update device data for ' + this.name,
-                no: 'Klarte ikke å oppdatere enhetsdata for ' + this.name
-            }),
-                this.name, 'ERROR');
+        if (this.polestar == null) {
+            let PolestarUser = this.homey.settings.get('user_email');
+            let PolestarPwd = await HomeyCrypt.decrypt(this.homey.settings.get('user_password'), PolestarUser);
+            this.polestar = new Polestar(PolestarUser, PolestarPwd);
+            await this.polestar.login();
+            await this.polestar.setVehicle(this.getData().vin);
         }
+        var odometer = await this.polestar.getOdometer();
+        console.log(JSON.stringify(odometer));
+        var odo = odometer.odometerMeters;
+        try {
+            odo = odo / 1000; //Convert to KM instead of M
+        } catch {
+            odo = null;
+        }
+        console.log('KM:' + odo)
+        var batteryInfo = await this.polestar.getBattery();
+        console.log(JSON.stringify(batteryInfo));
 
-        this.homey.app.log(this.homey.__({
-            en: 'Device data updated successfully',
-            no: 'Enhetsdata ble oppdatert vellykket'
-        }),
-            this.name, 'DEBUG');
-        this.homey.app.log(this.homey.__({
-            en: 'Next update in ' + moment.duration(this.refreshInterval).humanize(),
-            no: 'Neste oppdatering om ' + moment.duration(this.refreshInterval).humanize()
-        }),
-            this.name, 'DEBUG');
+        this.setCapabilityValue('measure_battery', batteryInfo.batteryChargeLevelPercentage);
+        // this.setCapabilityValue('measure_current', batteryInfo.chargingCurrentAmps);
+        // this.setCapabilityValue('measure_power', batteryInfo.chargingPowerWatts);
+        this.setCapabilityValue('measure_polestarOdometer', odo);
+
+        this.setCapabilityValue('measure_polestarRange', batteryInfo.estimatedDistanceToEmptyKm);
+        if (batteryInfo.chargingStatus == 'CHARGING_STATUS_CHARGING') {
+            this.setCapabilityValue('measure_polestarChargeState', true);
+            this.setCapabilityValue('measure_polestarChargeTimeRemaining', batteryInfo.estimatedChargingTimeToFullMinutes);
+        } else {
+            this.setCapabilityValue('measure_polestarChargeState', false);
+            this.setCapabilityValue('measure_polestarChargeTimeRemaining', null);
+        }
+        if (batteryInfo.chargerConnectionStatus == 'CHARGER_CONNECTION_STATUS_CONNECTED') {
+            this.setCapabilityValue('measure_polestarConnected', true);
+        } else {
+            this.setCapabilityValue('measure_polestarConnected', false);
+        }
     }
 
     async onAdded() {
@@ -140,4 +138,4 @@ class PolestarBetaDevice extends Device {
 
 }
 
-module.exports = PolestarBetaDevice;
+module.exports = PS2Device;
